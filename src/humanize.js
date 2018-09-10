a4da7b15074a626cb0786e2e05b9da67ec259e9d
Tom Shaver <l3l_aze@yahoo.com>
2018-09-10 02:51:20 -0500

    Updating humanize.js for Standard

--- src/humanize.js
+++ src/humanize.js
@@ -5,13 +5,22 @@
  */
 function humanize (bytes) {
   const threshold = 1024
-  const units = ['kB','MB','GB','TB','PB','EB','ZB','YB']
+  const units = [
+    'kB',
+    'MB',
+    'GB',
+    'TB',
+    'PB',
+    'EB',
+    'ZB',
+    'YB'
+  ]
 
   var u = -1
   do {
     bytes /= threshold
     ++u
-  } while(Math.abs(bytes) >= threshold && u < units.length - 1)
+  } while (Math.abs(bytes) >= threshold && u < units.length - 1)
   return bytes.toFixed(1) + ' ' + units[ u ]
 }
 
